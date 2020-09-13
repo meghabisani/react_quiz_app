e104bd0d11688a3fc80bf14490419a7151b34967
Megha <megha.bisani@gmail.com>
2020-09-13 17:56:14 +1000

    Updates

---
+++ precache-manifest.09358e5580200993d59c3c4a7ba49e97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01313d2cb821067f1a982c3fa6af15ac",
+    "url": "/react_quiz_app/index.html"
+  },
+  {
+    "revision": "7ee2679de98f3e65476c",
+    "url": "/react_quiz_app/static/js/2.9de41ea1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_quiz_app/static/js/2.9de41ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1fa4660f99c905958d",
+    "url": "/react_quiz_app/static/js/main.79d4f7f6.chunk.js"
+  },
+  {
+    "revision": "4c30e931cdc61f7be972",
+    "url": "/react_quiz_app/static/js/runtime-main.07329ab1.js"
+  },
+  {
+    "revision": "e21e818115329eeda15b07cdc08f331a",
+    "url": "/react_quiz_app/static/media/pencil_bg.e21e8181.jpg"
+  }
+]);
